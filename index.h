3f9e18d1f5fd5b14195a7fde6219b2f70347252d
Valérian Rey <valerian.rey@gmail.com>
2026-08-03 01:05:28 +0200

    Add COO

--- index.h
+++ index.h
@@ -3,7 +3,9 @@
 #include <array>
 #include <cstddef>
 #include <functional>
+#include <stdexcept>
 #include <type_traits>
+#include <unordered_set>
 
 
 class Index {
@@ -30,6 +32,7 @@ public:
         }
     }
     size_t operator[](int i) const { return value[i]; }
+    bool operator==(const Multintdex& other) const { return value == other.value; }
 };
 
 template<typename T>
@@ -39,6 +42,19 @@ struct is_multintdex<Multintdex<NDIM, SIZE>> : std::true_type {};
 
 template<typename T>
 concept IsMultintdex = is_multintdex<T>::value;
+
+namespace std {
+    template<int NDIM, std::array<size_t, NDIM> SIZE>
+    struct hash<Multintdex<NDIM, SIZE>> {
+        std::size_t operator()(const Multintdex<NDIM, SIZE>& idx) const noexcept {
+            std::size_t seed = 0;
+            for (int i = 0; i < NDIM; ++i) {
+                seed ^= std::hash<size_t>{}(idx[i]) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+            }
+            return seed;
+        }
+    };
+}
 
 
 template<size_t SIZE>
@@ -56,3 +72,44 @@ struct is_intdex<Intdex<SIZE>> : std::true_type {};
 
 template<typename T>
 concept IsIntdex = is_intdex<T>::value;
+
+namespace std {
+    template<size_t SIZE>
+    struct hash<Intdex<SIZE>> {
+        std::size_t operator()(const Intdex<SIZE>& idx) const noexcept {
+            return std::hash<size_t>{}(idx.get());
+        }
+    };
+}
+
+template<typename T>
+requires IsIndex<T>
+class Powerset : public Index {
+private:
+    std::unordered_set<T> values;
+public:
+    Powerset() = default;
+    Powerset(std::initializer_list<T> init) : values(init) {}
+
+    bool contains(const T& value) const { return values.contains(value); }
+    size_t size() const { return values.size(); }
+    void insert(const T& value) { values.insert(value); }
+
+    auto begin() const { return values.begin(); }
+    auto end() const { return values.end(); }
+
+    bool operator==(const Powerset& other) const { return values == other.values; }
+};
+
+namespace std {
+    template<typename T>
+    struct hash<Powerset<T>> {
+        std::size_t operator()(const Powerset<T>& s) const noexcept {
+            std::size_t seed = 0;
+            for (const auto& elem : s) {
+                seed ^= std::hash<T>{}(elem);
+            }
+            return seed;
+        }
+    };
+}
--- mapping.h
+++ mapping.h
@@ -32,3 +32,32 @@ public:
         return mU(result);
     }
 };
+
+template<typename mV, typename mU>
+requires IsIndex<mV> && IsIndex<mU>
+class PowersetMapping : public IndexMapping<mV, Powerset<mU>> {};
+
+template<int NNZ, int NDIM, std::array<size_t, NDIM> SIZE_V, size_t SIZE_U>
+class COO : public PowersetMapping<Multintdex<NDIM, SIZE_V>, Intdex<SIZE_U>> {
+private:
+    std::array<std::array<size_t, NDIM>, NNZ> inverse_coords;
+public:
+    COO(std::array<std::array<size_t, NDIM>, NNZ> inverse_coords) : inverse_coords(inverse_coords) {}
+
+    virtual Powerset<Intdex<SIZE_U>> operator()(Multintdex<NDIM, SIZE_V> index) override {
+        Powerset<Intdex<SIZE_U>> result;
+        for (int j = 0; j < NNZ; j++) {
+            bool match = true;
+            for (int i = 0; i < NDIM; i++) {
+                if (inverse_coords[j][i] != index[i]) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) {
+                result.insert(Intdex<SIZE_U>(j));
+            }
+        }
+        return result;
+    }
+};
